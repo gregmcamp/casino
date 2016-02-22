b783ac24eb9fe098afe08f8b19e7ab2d06827eaf
Greg Camp <gregmcamp@gmail.com>
2016-02-22 16:14:45 -0700

    update slots

--- slots.rb
+++ slots.rb
@@ -1,7 +1,6 @@
 class Slot_machine
-	attr_accessor :player
 
-	def initialize
+	def initialize(player)
 		puts "\nWelcome to the slot machine biotch!\n\n"
 		puts "yank my crank!!!\n\n"
 		puts "press the enter/return to continue"
@@ -10,7 +9,7 @@ class Slot_machine
 	end	
 
 
-	def slots(player)
+	def slots
 		slot_arr = %w[1 3 5 7 9]
 		result_1 = slot_arr.sample
 		result_2 = slot_arr.sample
@@ -20,19 +19,19 @@ class Slot_machine
 
 		if result_1 == result_2 && result_1 == result_3
 			puts "Congrats you hit the jackpot mother effer!!!\n\n"
-			player.bankroll += 100
+			@player.bankroll += 100
 		elsif result_1 == result_2
 			puts "Nice, you won a little somptin somptin\n\n"
-			player.bankroll += 20
+			@player.bankroll += 20
 		elsif result_1 == result_3
 			puts "Good for you dummy!\n\n"
-			player.bankroll += 20
+			@player.bankroll += 20
 		elsif result_2 == result_3
 			puts "Hooray for you, you won some money!\n\n"
-			player.bankroll += 20
+			@player.bankroll += 20
 		else 
 			puts "Wow, you really suck at this #{@name}!\n\n"
-			player.bankroll -= 10
+			@player.bankroll -= 10
 		end
 			exit_to_main
 	end	
@@ -45,7 +44,7 @@ class Slot_machine
 			slots
 		else
 			puts "Adios, thanks for the money!!!\n\n"
-			Casino.menu
+			casino.menu
 		end
 	end
 
